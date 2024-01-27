4579342501577d9916b4ba39486b6036e35eb1a8
xiaozhouwang <xiaozhou@ualberta.ca>
2024-01-27 13:24:31 -0500

    fix temporal up- down- sampling

--- models/stunet.py
+++ models/stunet.py
@@ -2,9 +2,7 @@
 
 """
 TODO: 
-1. 1d attention dim depends on the input pixel/latent size?
-2. Linear Projection layer in the inflation blocks?? (currently not implemented)
-3. initializing the trainable modules such that they perform nearest-neighbor down- and up- sampling operations 
+initializing the trainable modules such that they perform nearest-neighbor down- and up- sampling operations 
 results with a good starting point (in terms of the loss function)?? (not implemented)
 """
 
@@ -17,10 +15,13 @@ from diffusers.models.unet_2d_condition import UNet2DConditionOutput
 
 
 class STUNet(UNet2DConditionModel):
-    def __init__(self, temporal_attn_n_heads=8, temporal_attn_layers=4, *args, **kwargs):
+    def __init__(self, temporal_attn_n_heads=8, temporal_attn_layers=4, debug_verbose: bool = False,
+                 temporal_sampling: bool = True, *args, **kwargs):
         """
         :param temporal_attn_n_heads:
         :param temporal_attn_layers:
+        :param debug_verbose: print out information like intermediate results for debugging purposes
+        :param temporal_sampling: turn off to not perform temporal downsampling, e.g. to test if t2i base model works as expected
         :param args:
         :param kwargs:
         """
@@ -34,11 +35,14 @@ class STUNet(UNet2DConditionModel):
 
         """inflation blocks"""
         self.inflate_down_blocks = nn.ModuleList([])
+        self.temporal_downsample_blocks = nn.ModuleList([])
         self.inflate_mid_blocks = nn.ModuleList([])
         self.inflate_up_blocks = nn.ModuleList([])
+        self.temporal_upsample_blocks = nn.ModuleList([])
         """down, conv inflation blocks"""
         for block_out_c in block_out_channels:
             self.inflate_down_blocks.append(ConvInflationBlock(block_out_c, block_out_c, block_out_c))
+            self.temporal_downsample_blocks.append(temporal_resizing(block_out_c, sampling='down'))
         """middle, 1d attention layers"""
         for _ in range(temporal_attn_layers):
             self.inflate_mid_blocks.append(TemporalAttention(num_channels=block_out_channels[-1],
@@ -46,6 +50,10 @@ class STUNet(UNet2DConditionModel):
         """up, conv inflation blocks"""
         for block_out_c in block_out_channels[::-1]:
             self.inflate_up_blocks.append(ConvInflationBlock(block_out_c, block_out_c, block_out_c))
+            self.temporal_upsample_blocks.append(temporal_resizing(block_out_c, sampling='up'))
+
+        self.debug_verbose = debug_verbose
+        self.temporal_sampling = temporal_sampling
 
     def forward(
         self,
@@ -56,11 +64,20 @@ class STUNet(UNet2DConditionModel):
         attention_mask: Optional[torch.Tensor] = None,
         return_dict: bool = True,
     ) -> Union[UNet2DConditionOutput, Tuple]:
-        """input sample shape: b, c, d, h, w"""
+        """
+        :param sample:
+        :param timestep:
+        :param encoder_hidden_states:
+        :param class_labels:
+        :param attention_mask:
+        :param return_dict:
+        :return:
+        """
+        debug_verbose = self.debug_verbose
+        temporal_sampling = self.temporal_sampling
+
         b, c, d, h, w = sample.shape
         sample = rearrange(sample, 'b c d h w -> (b d) c h w') # combine batch and frame dim for 2d unet processing
-        encoder_hidden_states = repeat(encoder_hidden_states, 'b s e -> b d s e', d=d) # repeat text for each frame
-        encoder_hidden_states = rearrange(encoder_hidden_states, 'b d s e -> (b d) s e') # combine for 2d unet
 
         #####################################################
         ## Copy and Paste from UNET2DConditionModel.forward()
@@ -101,26 +118,6 @@ class STUNet(UNet2DConditionModel):
         elif len(timesteps.shape) == 0:
             timesteps = timesteps[None].to(sample.device)
 
-        # broadcast to batch dimension in a way that's compatible with ONNX/Core ML
-        timesteps = timesteps.expand(sample.shape[0])
-
-        t_emb = self.time_proj(timesteps)
-
-        # timesteps does not contain any weights and will always return f32 tensors
-        # but time_embedding might actually be running in fp16. so we need to cast here.
-        # there might be better ways to encapsulate this.
-        t_emb = t_emb.to(dtype=self.dtype)
-        emb = self.time_embedding(t_emb)
-
-        if self.class_embedding is not None:
-            if class_labels is None:
-                raise ValueError("class_labels should be provided when num_class_embeds > 0")
-
-            if self.config.class_embed_type == "timestep":
-                class_labels = self.time_proj(class_labels)
-
-            class_emb = self.class_embedding(class_labels).to(dtype=self.dtype)
-            emb = emb + class_emb
 
         # 2. pre-process
         sample = self.conv_in(sample)
@@ -128,24 +125,60 @@ class STUNet(UNet2DConditionModel):
         ## Copy and Paste done!
         #####################################################
         """adding conv and 1d attention layers to inflate now!"""
+        def prepare_encoder_hidden_states(encoder_hidden_states, num_frames):
+            new_hidden_states = repeat(encoder_hidden_states, 'b s e -> b d s e', d=num_frames) # repeat text for each frame
+            new_hidden_states = rearrange(new_hidden_states, 'b d s e -> (b d) s e') # combine for 2d unet
+            return new_hidden_states
+
+        def prepare_time_embedding(timesteps, batch_size, class_labels=class_labels):
+            # broadcast to batch dimension in a way that's compatible with ONNX/Core ML
+            new_timesteps = timesteps.expand(batch_size)
+
+            t_emb = self.time_proj(new_timesteps)
+
+            # timesteps does not contain any weights and will always return f32 tensors
+            # but time_embedding might actually be running in fp16. so we need to cast here.
+            # there might be better ways to encapsulate this.
+            t_emb = t_emb.to(dtype=self.dtype)
+            emb = self.time_embedding(t_emb)
+
+            if self.class_embedding is not None:
+                if class_labels is None:
+                    raise ValueError("class_labels should be provided when num_class_embeds > 0")
+
+                if self.config.class_embed_type == "timestep":
+                    class_labels = self.time_proj(class_labels)
+
+                class_emb = self.class_embedding(class_labels).to(dtype=self.dtype)
+                emb = emb + class_emb
+            return emb
 
         # 3. down
         """for each down block, we inflate with a inflate_down_block"""
         down_block_res_samples = (sample,)
-        for downsample_block, inflate_block in zip(self.down_blocks, self.inflate_down_blocks):
+        old_w = w
+        for downsample_block, inflate_block, sample_block in zip(self.down_blocks, self.inflate_down_blocks, self.temporal_downsample_blocks):
             if hasattr(downsample_block, "has_cross_attention") and downsample_block.has_cross_attention:
                 sample, res_samples = downsample_block(
                     hidden_states=sample,
-                    temb=emb,
-                    encoder_hidden_states=encoder_hidden_states,
+                    temb=prepare_time_embedding(timesteps, sample.shape[0]),
+                    encoder_hidden_states=prepare_encoder_hidden_states(encoder_hidden_states, num_frames=d),
                     attention_mask=attention_mask,
                 )
             else:
-                sample, res_samples = downsample_block(hidden_states=sample, temb=emb)
+                sample, res_samples = downsample_block(hidden_states=sample, temb=prepare_time_embedding(timesteps, sample.shape[0]))
 
             sample = rearrange(sample, '(b d) c h w -> b c d h w', b=b, d=d)
             inflated_sample = inflate_block(sample)
             sample = sample + inflated_sample
+            if debug_verbose:
+                print(f"down, old w: {old_w}, new w: {sample.shape[-1]}, current d: {d}")
+            if temporal_sampling:
+                if sample.shape[-1] != old_w:
+                    """downsample at frame level"""
+                    sample = sample_block(sample)
+                    d = d // 2 # half the number of frames
+                    old_w = sample.shape[-1]
             sample = rearrange(sample, 'b c d h w -> (b d) c h w')
             res_samples = res_samples[:-1] + (sample,)
 
@@ -153,7 +186,7 @@ class STUNet(UNet2DConditionModel):
 
         # 4. mid
         sample = self.mid_block(
-            sample, emb, encoder_hidden_states=encoder_hidden_states, attention_mask=attention_mask
+            sample, prepare_time_embedding(timesteps, sample.shape[0]), encoder_hidden_states=prepare_encoder_hidden_states(encoder_hidden_states, num_frames=d), attention_mask=attention_mask
         )
         """inflate with 1d attention layers"""
         sample = rearrange(sample, '(b d) c h w -> b c d h w', b=b, d=d)
@@ -162,7 +195,8 @@ class STUNet(UNet2DConditionModel):
         sample = rearrange(sample, 'b c d h w -> (b d) c h w')
 
         # 5. up
-        for i, (inflate_block, upsample_block) in enumerate(zip(self.inflate_up_blocks, self.up_blocks)):
+        old_w = sample.shape[-1]
+        for i, (inflate_block, upsample_block, sample_block) in enumerate(zip(self.inflate_up_blocks, self.up_blocks, self.temporal_upsample_blocks)):
             is_final_block = i == len(self.up_blocks) - 1
 
             res_samples = down_block_res_samples[-len(upsample_block.resnets) :]
@@ -176,21 +210,30 @@ class STUNet(UNet2DConditionModel):
             if hasattr(upsample_block, "has_cross_attention") and upsample_block.has_cross_attention:
                 sample = upsample_block(
                     hidden_states=sample,
-                    temb=emb,
+                    temb=prepare_time_embedding(timesteps, sample.shape[0]),
                     res_hidden_states_tuple=res_samples,
-                    encoder_hidden_states=encoder_hidden_states,
+                    encoder_hidden_states=prepare_encoder_hidden_states(encoder_hidden_states, num_frames=d),
                     upsample_size=upsample_size,
                     attention_mask=attention_mask,
                 )
             else:
                 sample = upsample_block(
-                    hidden_states=sample, temb=emb, res_hidden_states_tuple=res_samples, upsample_size=upsample_size
+                    hidden_states=sample, temb=prepare_time_embedding(timesteps, sample.shape[0]), res_hidden_states_tuple=res_samples, upsample_size=upsample_size
                 )
 
             """inflate up block"""
             sample = rearrange(sample, '(b d) c h w -> b c d h w', b=b, d=d)
             inflated_sample = inflate_block(sample)
             sample = sample + inflated_sample
+            if debug_verbose:
+                print(f"up, old w: {old_w}, new w: {sample.shape[-1]}, current d: {d}")
+            if temporal_sampling:
+                if sample.shape[-1] != old_w:
+                    """upsample at frame level"""
+                    sample = sample_block(sample)
+                    d = d * 2 # double the number of frames
+                    old_w = sample.shape[-1]
+
             sample = rearrange(sample, 'b c d h w -> (b d) c h w')
 
         # 6. post-process
@@ -211,24 +254,32 @@ class STUNet(UNet2DConditionModel):
             param.requires_grad = False
 
         # Unfreeze the inflation blocks
-        for block in self.inflate_down_blocks:
-            for param in block.parameters():
-                param.requires_grad = True
+        trainable_blocks = [self.inflate_down_blocks, self.temporal_downsample_blocks,
+                            self.inflate_up_blocks, self.temporal_upsample_blocks,
+                            self.inflate_mid_blocks]
+        for blocks in trainable_blocks:
+            for block in blocks:
+                for param in block.parameters():
+                    param.requires_grad = True
 
-        for block in self.inflate_mid_blocks:
-            for param in block.parameters():
-                param.requires_grad = True
 
-        for block in self.inflate_up_blocks:
-            for param in block.parameters():
-                param.requires_grad = True
+def temporal_resizing(channels, sampling):
+    if sampling == "down":
+        # layer = [nn.AvgPool3d(kernel_size=(2, 1, 1), stride=(2, 1, 1))]
+        layer = nn.Conv3d(channels, channels, kernel_size=(3, 1, 1), stride=(2, 1, 1), padding=(1, 0, 0),
+                             bias=False)
+    elif sampling == "up":
+        layer = nn.ConvTranspose3d(channels, channels, (4, 1, 1), (2, 1, 1), (1, 0, 0), bias=False)
+    else:
+        raise ValueError(f"Sampling must be either None, 'down' or 'up', but got {sampling}")
+
+    return layer
 
 
 class ConvInflationBlock(nn.Sequential):
     """Convolution-based Inflation Block in Lumiere, without the pretrained spatial layers"""
     def __init__(self, in_planes: int, out_planes: int, midplanes: int, stride: int = 1, padding: int = 1) -> None:
-        super().__init__(
-            nn.Conv3d(
+        layers = [nn.Conv3d(
                 in_planes,
                 midplanes,
                 kernel_size=(1, 3, 3),
@@ -242,7 +293,10 @@ class ConvInflationBlock(nn.Sequential):
                 midplanes, out_planes, kernel_size=(3, 1, 1), stride=(stride, 1, 1), padding=(padding, 0, 0), bias=False
             ),
             nn.BatchNorm3d(out_planes),
-            nn.SiLU(inplace=True),
+            nn.SiLU(inplace=True),]
+
+        super().__init__(
+            *layers
         )
 
 
@@ -290,11 +344,11 @@ if __name__ == "__main__":
     model_id = "stabilityai/stable-diffusion-2-1"
     unet = UNet2DConditionModel.from_pretrained(model_id, subfolder="unet")
     unet_config = dict(unet.config)
-    stunet = STUNet(temporal_attn_layers=5, **unet_config)
+    stunet = STUNet(temporal_attn_layers=5, debug_verbose=True, temporal_sampling=False, **unet_config)
     latents = torch.randn(
-        (1, unet.in_channels, 80, 32, 32)
+        (3, unet.in_channels, 16, 16, 16)
     ).to('cuda')
-    text_embeddings = torch.randn(1, 77, 1024).to('cuda')  # 1024 is the dim used in sd2.1
+    text_embeddings = torch.randn(3, 77, 1024).to('cuda')  # 1024 is the dim used in sd2.1
     t = torch.tensor(50).long().to('cuda')
     stunet = stunet.to('cuda')
     out = stunet(latents, t, text_embeddings)
